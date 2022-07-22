6f39c753b7598bf0f00ba21cbbd8863aa14d39cb
Stenli777 <alekseysten777@yandex.ru>
2022-07-22 08:32:01 +0300

    update

--- index.php
+++ index.php
@@ -11,25 +11,24 @@ require __DIR__ . '/../vendor/autoload.php';
 // Instantiate App
  $app = AppFactory::create();
 
-// Add error middleware
+  // Add error middleware
  $app->addErrorMiddleware(true, true, true);
 
-// Add routes
+   // Add routes
  $app->get('/', function (Request $request, Response $response) {
      $response->getBody()->write('<a href="/hello/world">Try /hello/world</a>');
-         return $response;
+          return $response;
+          });
+
+     $app->get('/hello/{name}', function (Request $request, Response $response, $args) {
+          $name = $args['name'];
+          $response->getBody()->write("Hello, $name");
+          return $response;
+          });
+
+     $app->get('/about', function ($request, $response) {
+         $phpView = new PhpRenderer(__DIR__ . '/../templates');
+         return $phpView->render($response, 'about.phtml');
          });
 
-         $app->get('/hello/{name}', function (Request $request, Response $response, $args) {
-             $name = $args['name'];
-                 $response->getBody()->write("Hello, $name");
-                     return $response;
-                     });
-
-      $app->get('/about', function ($request, $response) {
-	                $phpView = new PhpRenderer('../templates');
-			          return $phpView->render($response, 'about.phtml');
-			      });
-
-                     $app->run();
-                     
+                                                                                                                  	                			          			                           $app->run();
